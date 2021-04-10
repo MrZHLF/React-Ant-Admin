a8b43bf876554d37245884f9176872752ef8a690
MrZHLF <480970024@qq.com>
2021-04-10 21:06:03 +0800

    feat:antd引入以及scss配使用

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React,{Component,Fragment} from 'react'
 import './App.scss';
+import { Button } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>6666</h1>
-    </div>
-  );
+class Home extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <Fragment>
+        <h1>6666</h1>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+      </Fragment>
+    )
+  }
 }
-
-export default App;
+export default Home;
